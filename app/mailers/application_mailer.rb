7b0ea823a0494356d65000c83ec85a66954e1dc6
Amol Pujari <amolpujari@gmail.com>
2018-04-11 23:59:57 +0530

    notify cc

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
   include ApplicationHelper
 
-  default from: "Sine <info@sine>"
+  default from: "Sine <info@sine>", cc: [Rails.application.secrets.notify[:cc].to_s.split(',')].flatten.compact.map(&:to_s).map(&:strip).uniq
   layout 'mailer'
 
   def notify record, options={}
@@ -17,7 +17,6 @@ class ApplicationMailer < ActionMailer::Base
     @body = options.delete(:body) || @record.to_email    
     
     mail(to: [record.emails].flatten.compact.map(&:to_s).map(&:strip).uniq, 
-      cc: [Rails.application.secrets.notify[:after_create].to_s.split(',')].flatten.compact.map(&:to_s).map(&:strip).uniq,
       subject: @subject
     )
   end
